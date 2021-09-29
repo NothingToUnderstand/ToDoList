28d2f6414f133a8d1e418077b75fefbbba8bb01a
NothingToUnderstand <91472828+NothingToUnderstand@users.noreply.github.com>
2021-09-29 10:43:18 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,69 @@
+
+let addMessage=document.querySelector('.message'),
+ addButton=document.querySelector('.add'),
+ todo=document.querySelector('.todo');
+
+ let todoList=[];
+
+ if(localStorage.getItem("todo")){
+     todoList=JSON.parse(localStorage.getItem("todo"));
+    displayMessages();
+    }
+
+addButton.addEventListener('click',function(){
+    if(!addMessage.value)return;
+    let newTodo={
+        todo:addMessage.value,
+        checked:false,
+        important:false
+    }
+     todoList.push(newTodo);
+     displayMessages();
+     localStorage.setItem("todo",JSON.stringify(todoList));
+     addMessage.value="";
+});
+
+
+function displayMessages(){
+    let displayMessages="";
+    todoList.forEach(function(item,i){
+        displayMessages +=`
+       <li>
+       <input type='checkbox' id='item_${i}' ${item.checked ? "checked":""}>
+       <label for="item_${i}" class="${item.important ? "important":""}">${item.todo}</label>
+       </li>
+        `;
+        todo.innerHTML=displayMessages;
+    })
+}
+   todo.addEventListener('change',function(event){
+    let idInput= event.target.getAttribute('id');
+    let forLable=todo.querySelector('[for='+idInput+']');
+    let valueLabel=forLable.innerHTML;
+    console.log('valueLabel:',valueLabel);
+    todoList.forEach(function(item){
+        if(item.todo===valueLabel){
+            item.checked=!item.checked;
+            localStorage.setItem("todo",JSON.stringify(todoList));
+
+        }
+    })
+    })
+
+    todo.addEventListener('contextmenu',function(event){
+        event.preventDefault();
+        todoList.forEach(function(item,i){
+            if(item.todo===event.target.innerHTML){
+                if(event.ctrlKey || event.metaKey){
+                    todoList.splice(i);
+                    if(todoList.length === 0) {
+                        todo.innerHTML= "";
+                        localStorage.clear();
+                    }
+                }else{
+                     item.important=!item.important;
+                displayMessages();
+                localStorage.setItem("todo",JSON.stringify(todoList));}
+            }
+        })
+    })
